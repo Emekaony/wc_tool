050aef3e2ab2682586ffd2fff9d73fdfa7a538fc
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-05-26 05:30:12 -0700

    Code looks a lot cleaner now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// import 'dart:async';
-import 'dart:io';
+import 'util/display_line_count.dart';
+import 'util/display_word_count.dart';
+import 'util/display_byte_count.dart';
 
 void main(List<String> args) async {
   String flag = args[0];
@@ -17,37 +18,4 @@ void main(List<String> args) async {
     default:
       print("flag unsupported");
   }
-}
-
-/// get the number of bytes in a specified file
-void displayBytes(String fileName) async {
-  final file = File("lib/data/$fileName");
-  final bytes = await file.readAsBytes();
-  print("${bytes.length} $fileName");
-}
-
-void displayLineCount(String fileName) async {
-  final file = File("lib/data/$fileName");
-  final lines = await file.readAsLines();
-  int lineCount = 0;
-  for (String _ in lines) {
-    lineCount++;
-  }
-  print("$lineCount $fileName");
-}
-
-void displayWordCount(String fileName) async {
-  int wordCount = 0;
-  final file = File("lib/data/$fileName");
-  List<String> lines = await file.readAsLines();
-  for (String line in lines) {
-    String trimmedLine = line.trim();
-    if (trimmedLine.isNotEmpty) {
-      List<String> words =
-          trimmedLine.split(" ").where((word) => word.isNotEmpty).toList();
-
-      wordCount += words.length;
-    }
-  }
-  print("$wordCount $fileName");
 }
