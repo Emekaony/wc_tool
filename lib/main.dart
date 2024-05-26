acb4699b942322c821e855a87d8532d3c78d0e4f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-05-26 04:07:25 -0700

    Refactored the file to just parse cli args myself

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
-import 'dart:convert';
 // import 'dart:async';
 import 'dart:io';
 
-void main() async {
-  final file = File("lib/test.txt");
+void main(List<String> args) async {
+  String flag = args[0];
+  String fileName = args[1];
+
+  switch (flag) {
+    case '-c':
+      displayBytes(fileName);
+    default:
+      print("flag unsupported");
+  }
+}
+
+/// get the number of bytes in a specified file
+void displayBytes(String fileName) async {
+  final file = File("lib/data/$fileName");
   final bytes = await file.readAsBytes();
   print("Number of bytes: ${bytes.length}");
 }
